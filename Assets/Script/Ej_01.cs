dd210ad5bfe2aeab5c20a143166aeace42a77a0a
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-03-31 08:41:45 -0300

    Ejercicio 2

--- Assets/Script/Ej_01.cs
+++ Assets/Script/Ej_01.cs
@@ -7,7 +7,11 @@ public class Ej_01 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //1. Realizá un programa donde se cree una variable entera llamada num1, que inicialmente valdrá 0.Luego incrementá su valor en 2 y mostralo por pantalla. Después mostrá el resultado de multiplicarlo por sí mismo.
+        int num1 = 0;
+        num1 += 2;
+        Debug.Log(num1);
+        Debug.Log(num1 * num1);
     }
 
     // Update is called once per frame
