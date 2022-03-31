5b49b079ef4931c31fa1929c3a2297947d5eb554
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-03-31 09:02:01 -0300

    Ejercicio 4

--- Assets/Script/Ej_03.cs
+++ Assets/Script/Ej_03.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//3. Crear una variable pública que permita ingresar texto. Permitir que al ejecutar el programa
+//se muestre en la consola un mensaje de saludo que incluya el texto de la variable. Por
+//ejemplo, si en la variable se ingresa el texto “Ana”, el mensaje en consola debe decir “Hola Ana”. 
 
-public class NewBehaviourScript : MonoBehaviour
+public class Ej_03 : MonoBehaviour
 {
+    public string nombre;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hola " + nombre);
     }
 
     // Update is called once per frame
---
+++ Assets/Script/Ej_04.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//4.. Realizá un programa que permita ingresar dos números enteros num1 y num2 por
+//Inspector.Luego, mostrar el resultado de la suma entre ambos al ejecutar la escena
+//utilizando el siguiente formato: “La suma entre “ + num1 + “ y “ + num2 + “ da como
+//resultado “ + num1 + num2.
+public class Ej_04 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int res = num1 + num2;
+        Debug.Log("La suma entre " + num1 + " y " + num2 + " da como resultado " + res);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
