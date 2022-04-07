945da5d1895f7b83c087f66a8a9ad70a9ebfbfd4
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-04-07 08:14:31 -0300

    Ejercicio 08 por la mitad

---
+++ Assets/Script/Ej_08.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//8. Crear una variable entera que pueda ser modificada desde el Inspector.Suponiendo que
+//el primer día de la semana es el Domingo, mostrar el nombre del día correspondiente al
+//valor de la variable “dia”. Si el día no está en el rango permitido debe emitir por pantalla el
+//mensaje de error "El día ingresado no es válido".
+public class Ej_08 : MonoBehaviour
+{
+    public int dia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (dia)
+        {
+            case 1:
+                Debug.Log("Domingo");
+                break;
+            case 2:
+                Debug.Log("Lunes");
+                break;
+            case 3:
+                Debug.Log("Martes");
+                break;
+            case 4:
+                Debug.Log("Miércoles");
+                break;
+            case 5:
+                Debug.Log("Jueves");
+                break;
+            default:
+                numero = "error";
+                break;
+        }
+        if (dia==1)
+        {
+            Debug.Log("No se puede dividir por cero");
+        }
+        else
+        {
+            res = num1 / num2;
+            Debug.Log(num1 + " / " + num2 + " = " + res);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
